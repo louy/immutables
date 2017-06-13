902ad3c8cecf2173e48ffe3e73331f0eabfa5e05
Louay Alakkad <louay_alakkad@mckinsey.com>
2017-06-13 16:09:45 +0100

    getIn and setIn

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,6 +1,6 @@
 /// <reference types="jest" />
 
-import {get, set} from '../index'
+import {get, set, getIn, setIn} from '../index'
 
 describe('get', () => {
   it('works', () => {
@@ -34,5 +34,45 @@ describe('set', () => {
       test2: true,
     })
     expect((object as any).test2).toEqual(void 0)
+  })
+})
+
+describe('getIn', () => {
+  it('works with nested objects', () => {
+    expect(
+      getIn({
+        test: {test2: {test3: 123}}
+      }, ['test', 'test2', 'test3'])
+    ).toEqual(123)
+  })
+
+  it('works with missing objects', () => {
+    expect(
+      getIn({
+        test: {test2: {test3: 123}}
+      }, ['test', 'test2', 'test3', 'test4'])
+    ).toEqual(void 0)
+  })
+})
+
+describe('setIn', () => {
+  it('works with nested objects', () => {
+    const object = {test: {}}
+    expect(
+      setIn(object, ['test', 'test2'], 123)
+    ).toEqual({
+      test: {test2: 123}
+    })
+    expect((object.test as any).test2).toEqual(void 0)
+  })
+
+  it('works with missing objects', () => {
+    const object = {}
+    expect(
+      setIn(object, ['test', 'test2'], 123)
+    ).toEqual({
+      test: {test2: 123}
+    })
+    expect((object as any).test).toEqual(void 0)
   })
 })
--- index.ts
+++ index.ts
@@ -4,10 +4,29 @@ export function get<T, K extends keyof T>(object: T, key: K): T[K] {
 
 export function set<T, K extends keyof T>(object: T, key: K, value: T[K]): T;
 export function set<T, K extends string, V>(object: T, key: K, value: V): T & { [key in K]: V};
-export function set(object, key, value) {
+export function set(object: any, key: string, value: any) {
   return Object.freeze(
     object[key] === value
     ? object
     : Object.assign({},object,{[key]:value})
+  )
+}
+
+export function getIn<T, K1 extends keyof T>(object: T, keys: [K1]): T[K1];
+export function getIn<T, K1 extends keyof T, K2 extends keyof T[K1]>(object: T, keys: [K1, K2]): T[K1][K2];
+export function getIn<T, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2]>(object: T, keys: [K1, K2, K3]): T[K1][K2][K3];
+export function getIn<T, K1 extends keyof T, K2 extends keyof T[K1], K3 extends keyof T[K1][K2], K4 extends keyof T[K1][K2][K3]>(object: T, keys: [K1, K2, K3, K4]): T[K1][K2][K3][K4];
+export function getIn(object: any, keys: string[]): any;
+export function getIn(object: any, keys: string[]): any {
+  return keys.reduce((object, key) => object && get(object, key), object)
+}
+
+export function setIn(object: any = {}, keys: string[], value: any): any {
+  if (keys.length === 1) return set(object, keys[0], value)
+
+  return set(
+    object,
+    keys[0],
+    setIn(get(object, keys[0]), keys.slice(1), value)
   )
 }
