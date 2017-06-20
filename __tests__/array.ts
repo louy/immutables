93bebb4009a2326952ef60d9bd5dce93a6044ab0
Louay Alakkad <louay_alakkad@mckinsey.com>
2017-06-20 17:39:59 +0100

    Array fill method

--- __tests__/array.ts
+++ __tests__/array.ts
@@ -1,6 +1,6 @@
 /// <reference types="jest" />
 
-import { push, pop, shift, unshift } from '../array';
+import { push, pop, shift, unshift, fill } from '../array';
 
 describe('push', () => {
   it('adds item to bottom of array', () => {
@@ -55,5 +55,18 @@ describe('shift', () => {
 
     expect(array.length).toEqual(2);
     expect(array2.length).toEqual(1);
+  });
+});
+
+describe('fill', () => {
+  it('fills array as expected', () => {
+    expect(fill(['test1', 'test2', 'test3'], 123)).toEqual([123, 123, 123]);
+    expect(fill(['test1', 'test2', 'test3'], 123, 1, 2)).toEqual(['test1', 123, 'test3']);
+  });
+  it("doesn't modify original array", () => {
+    const array = ['test', 'test2'];
+    const array2 = fill(array, 'test3');
+    expect(array).toEqual(['test', 'test2']);
+    expect(array2).toEqual(['test3', 'test3']);
   });
 });
--- array.ts
+++ array.ts
@@ -1,21 +1,46 @@
+function clone<T>(array: ArrayLike<T>): Array<T> {
+  return Array.prototype.slice.call(array);
+}
+
 export function push<T>(array: ArrayLike<T>, item: T): ReadonlyArray<T> {
-  const sliced: T[] = Array.prototype.slice.call(array);
+  const sliced: T[] = clone(array);
   sliced.push(item);
   return Object.freeze(sliced);
 }
 export function pop<T>(array: ArrayLike<T>): ReadonlyArray<T> {
-  const sliced: T[] = Array.prototype.slice.call(array);
+  const sliced: T[] = clone(array);
   sliced.pop();
   return Object.freeze(sliced);
 }
 
 export function shift<T>(array: ArrayLike<T>): ReadonlyArray<T> {
-  const sliced: T[] = Array.prototype.slice.call(array);
+  const sliced: T[] = clone(array);
   sliced.shift();
   return Object.freeze(sliced);
 }
 export function unshift<T>(array: ArrayLike<T>, item: T): ReadonlyArray<T> {
-  const sliced: T[] = Array.prototype.slice.call(array);
+  const sliced: T[] = clone(array);
   sliced.unshift(item);
   return Object.freeze(sliced);
+}
+
+export function fill<T>(
+  array: ArrayLike<any>,
+  item: T
+): ReadonlyArray<T>;
+
+export function fill<T1, T2>(
+  array: ArrayLike<T1>,
+  item: T2,
+  start: number,
+  end?: number
+): ReadonlyArray<T1 | T2>;
+
+export function fill(
+  array: ArrayLike<any>,
+  item: any,
+  start?: number,
+  end?: number,
+): ReadonlyArray<any> {
+  return Object.freeze(clone(array).fill(item, start, end));
 }
